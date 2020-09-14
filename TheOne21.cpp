37d9d1584fc196d5c38618f506bcfef6bff37bbd
POG <lyazg10@mail.com>
2020-09-14 22:24:27 +0500

    Add TheOne21

---
+++ TheOne21.cpp
@@ -0,0 +1,44 @@
+#include "pt4.h"
+using namespace std;
+
+void Solve()
+{
+    Task("TheOne21");
+	int a;
+	double b;
+	pt >> a;
+	pt >> b;
+	switch (a)
+	{
+	case 1:
+	{
+		pt << b;
+		break;
+	}
+	case 2:
+	{
+		pt << (b /= 1000000);
+		break;
+	}
+	case 3:
+	{
+		pt << (b /= 1000);
+		break;
+	}
+	case 4:
+	{
+		pt << (b *= 1000);
+		break;
+	}
+	case 5:
+	{
+		pt << (b *= 100);
+		break;
+	}
+	deafult:
+	{
+		pt << "error";
+		break;
+	}
+	}
+}
