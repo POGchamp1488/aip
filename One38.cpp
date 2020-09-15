d880896c7eb6c4fd45fd0ad4f78fd095635cd526
POG <lyazg10@mail.com>
2020-09-15 22:01:12 +0500

    add One38 improve_1

--- One38.cpp
+++ One38.cpp
@@ -1,16 +1,20 @@
 #include "pt4.h"
 using namespace std;
 
+int count_digits(int n) {
+	int k = 0;
+	do {
+		if ((n % 10) == 0)
+			k++;
+		n /= 10;
+	} while (n != 0);
+	return k;
+}
+
 void Solve()
 {
     Task("One38");
-	int n, b = 0;
+	int n;
 	pt >> n;
-	for (int i = 0; i <= 5; i++)
-	{
-		if ((n % 10) == 0)
-			b++;
-		n /= 10;
-	}
-	pt << b;
+	pt << count_digits(n);
 }
